1a2047799de073017f62af05ea761ee288923d3a
Marcos Cruz <mzt.macruz@voegol.com.br>
2024-07-22 17:18:43 -0300

    feat: add data annotations to validate createrestaurantDto

--- src/Bigai.Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ src/Bigai.Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -1,14 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bigai.Restaurants.Application.Restaurants.Dtos;
 
 public class CreateRestaurantDto
 {
+    [StringLength(100, MinimumLength = 3)]
     public string Name { get; set; } = default!;
+
     public string Description { get; set; } = default!;
+
+    [Required(ErrorMessage = "Inser a valida category")]
     public string Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
+
+    [EmailAddress(ErrorMessage = "Please provide a valid email addres")]
     public string? ContactEmail { get; set; }
+
+    [Phone(ErrorMessage = "Please provide a valid phone number")]
     public string? ContactNumber { get; set; }
+
     public string? City { get; set; }
+
     public string? Street { get; set; }
+
+    [RegularExpression(@"^\d\d{2}-\d\d{3}$", ErrorMessage = "Please provide a valid postal code (XX-XXX)")]
     public string? PostalCode { get; set; }
 }
